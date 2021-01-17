d9d80cd8f625a2575fdcca67417e58c24f5dffe6
rocio picaza <rocio.picaza@gmail.com>
2021-01-16 21:40:22 -0300

    more Style

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@ ingredient_1 = Ingredient.create(name: "lemon")
 ingredient_2 = Ingredient.create(name: "ice")
 ingredient_3 = Ingredient.create(name: "mint leaves")
 
-cocktail_1 = Cocktail.create(name: "Mojito", image: "https://images.unsplash.com/photo-1536935338788-846bb9981813?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8Y29ja3RhaWx8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60")
-cocktail_2 = Cocktail.create(name: "Sex on the Beach", image: "https://images.unsplash.com/photo-1563223771-5fe4038fbfc9?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8Y29ja3RhaWx8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60")
-cocktail_3 = Cocktail.create(name: "Whisky", image: "https://images.unsplash.com/photo-1587223962930-cb7f31384c19?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80")
+cocktail_1 = Cocktail.create(name: "Mojito", image: "https://dg38gzoej82xh.cloudfront.net/sites/default/files/styles/image_gallery_xl/public/images/node/article/beautiful-mojito.jpg?itok=2RgyMLU6")
+cocktail_2 = Cocktail.create(name: "Sex on the Beach", image: "https://noseychef.com/wp-content/uploads/2018/07/IMG_3744.jpg")
+cocktail_3 = Cocktail.create(name: "Whisky", image: "https://dg38gzoej82xh.cloudfront.net/sites/default/files/styles/image_gallery_xl/public/images/node/article/29-negroni_0.jpg?itok=2gtUtefp")
 
 Dose.create(description: "10gr", cocktail: cocktail_1, ingredient: ingredient_1)
 Dose.create(description: "20gr", cocktail: cocktail_2, ingredient: ingredient_2)
