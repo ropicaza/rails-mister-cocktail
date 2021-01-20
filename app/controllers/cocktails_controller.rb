a6b88a16b381af907bedf8b1fa01500cc7ec9e4e
rocio picaza <rocio.picaza@gmail.com>
2021-01-19 22:09:27 -0300

    Image tag with cloudinary ok

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -21,9 +21,15 @@ class CocktailsController < ApplicationController
     @dose = Dose.new
   end
 
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :image)
+    params.require(:cocktail).permit(:name, :image, :photo )
   end
 end
